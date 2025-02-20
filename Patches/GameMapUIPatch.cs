4a88ce0c41374a687a5207c1275de30ec9c08cc9
Sarah Robert <sarah.robert@etu.cegepdrummond.ca>
2025-02-19 20:02:58 -0500

    Find the good function to patch

---
+++ Patches/GameMapUIPatch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using HarmonyLib;
+
+namespace OriBFArchipelago.Patches;
+
+[HarmonyPatch(typeof(GameMapUI), nameof(GameMapUI.Show))]
+internal class GameMapUIPatch
+{
+    private static void Postfix(GameMapUI __instance)
+    {
+        Console.WriteLine("All Map is Discovered by Prefix!");
+    }
+}
