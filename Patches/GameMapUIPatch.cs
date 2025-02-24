d98fec92c1d676841651d5483125be53d2fec247
MysroHub <sarahrobert238@gmail.com>
2025-02-24 10:18:49 -0500

    Make the entire world map visible

---
+++ Patches/GameMapUIPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace OriBFArchipelago.Patches;
+
+/// <summary>
+/// Patch the GameMapUI to see the entire world map
+/// </summary>
+[HarmonyPatch(typeof(GameMapUI), nameof(GameMapUI.Show))]
+internal class GameMapUIPatch
+{
+    
+    /// <summary>
+    /// Allows you to see the entire world map
+    /// </summary>
+    /// <param name="__instance">the instance of the GameMapUI to patch</param>
+    private static void Postfix(GameMapUI __instance)
+    {
+        foreach (var teleporter in __instance.Teleporters.Teleporters)
+        {
+            teleporter.Area.VisitAllAreas();
+        }
+    }
+}
