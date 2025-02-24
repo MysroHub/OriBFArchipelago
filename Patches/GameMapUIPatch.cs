0b04b25d526eafb6e762b69be9e251641f3b9598
Sarah Robert <sarah.robert@etu.cegepdrummond.ca>
2025-02-24 09:54:18 -0500

    Make the entire world map visible

--- Patches/GameMapUIPatch.cs
+++ Patches/GameMapUIPatch.cs
@@ -1,14 +1,23 @@
-﻿using System;
-using HarmonyLib;
+﻿using HarmonyLib;
 
 namespace OriBFArchipelago.Patches;
 
+/// <summary>
+/// Patch the GameMapUI to see the entire world map
+/// </summary>
 [HarmonyPatch(typeof(GameMapUI), nameof(GameMapUI.Show))]
 internal class GameMapUIPatch
 {
+    
+    /// <summary>
+    /// Allows you to see the entire world map
+    /// </summary>
+    /// <param name="__instance">the instance of the GameMapUI to patch</param>
     private static void Postfix(GameMapUI __instance)
     {
-        Console.WriteLine("change map to visited all areas");
-        __instance.CurrentHighlightedArea.VisitAllAreas();
+        foreach (var teleporter in __instance.Teleporters.Teleporters)
+        {
+            teleporter.Area.VisitAllAreas();
+        }
     }
 }
