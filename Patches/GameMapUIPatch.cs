6ea0928022348cdbfc5bf57b489773325e20e3ba
MysroHub <sarahrobert238@gmail.com>
2025-03-02 09:07:39 -0500

    Try to find where the mist is set

--- Patches/GameMapUIPatch.cs
+++ Patches/GameMapUIPatch.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
 using Sein.World;
 
 namespace OriBFArchipelago.Patches;
@@ -13,8 +14,6 @@ internal class GameMapUIPatch
     /// <summary>
     /// Allows you to see the entire world map
     /// </summary>
-    /// <param name="__instance">the instance of the GameMapUI to patch</param>
-    
     private static void Prefix()
     {
         foreach (var area in GameWorld.Instance.RuntimeAreas)
@@ -30,7 +29,32 @@ internal class MistControllerPatch
 {
     private static void Prefix()
     {
+        Console.WriteLine("MistController called");
         Events.MistLifted = true;
     }
 }
+
+[HarmonyPatch(typeof(MistAction), nameof(MistAction.Perform))]
+internal class MistActionPatch
+{
+    private static void Prefix(MistAction __instance)
+    {
+        Console.WriteLine("MistAction called");
+        __instance.Action = MistAction.ActionType.HideMist;
+    }
+}
+
+[HarmonyPatch(typeof(MistyWoodsAreaMapCanvas), nameof(MistyWoodsAreaMapCanvas.OnEnable))]
+internal class MistyWoodsAreaMapCanvasPatch
+{
+    private static bool Prefix()
+    {
+        Console.WriteLine("MistyWoodsAreaMapCanvasPatch called");
+        return false; // We want to skip the original method
+    }
+}
+
+
+
+
 
