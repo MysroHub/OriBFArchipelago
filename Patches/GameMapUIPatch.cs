7481f344fc8a5bd8218556e2d3544a04b225a096
MysroHub <sarahrobert238@gmail.com>
2025-04-05 08:58:59 -0400

    Remove the fog from the map

--- Patches/GameMapUIPatch.cs
+++ Patches/GameMapUIPatch.cs
@@ -1,16 +1,17 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace OriBFArchipelago.Patches;
 
 /// <summary>
-/// Patch the GameMapUI to see the entire world map
+/// Patchs the GameMapUI to see the entire world map except Mysty Woods
 /// </summary>
 [HarmonyPatch(typeof(GameMapUI), nameof(GameMapUI.Show))]
 internal class GameMapUIPatch
 {
     
     /// <summary>
-    /// Allows you to see the entire world map
+    /// Allows you to see the entire world map except Misty Woods
     /// </summary>
     private static void Prefix()
     {
@@ -18,5 +19,23 @@ internal class GameMapUIPatch
         {
             area.VisitAllAreas();
         }
+    }
+}
+
+/// <summary>
+/// Patchs the Misty Woods Area of the GameMapUI
+/// We go through a routine that is active when we need to patch the map
+/// </summary>
+[HarmonyPatch(typeof(GameMapUI), nameof(GameMapUI.UpdateAreaText))]
+internal class UpdateAreaTextPath
+{
+    /// <summary>
+    /// Unlock the Misty Woods Area on the GameMapUI
+    /// </summary>
+    private static void Prefix()
+    {
+        Transform mapPivot = AreaMapUI.Instance.transform.Find("mapPivot");
+        mapPivot.FindChild("mistyWoodsFog").gameObject.SetActive(false);
+        mapPivot.FindChild("mistyWoods").gameObject.SetActive(true);
     }
 }
